9a23078f12fdcd11033a3cebab3cf0a126a14bb9
Ashree <ashreeash4@gmail.com>
2024-11-08 10:21:38 +0530

    Added buttons

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,9 @@
 import java.awt.Color;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Calculator {
     public Calculator() {
@@ -11,9 +13,47 @@ public class Calculator {
         jf.setLocation(300, 150);
         JLabel displayLabel = new JLabel("HELLO");
         displayLabel.setBounds(30, 50, 540, 40);
-        displayLabel.setBackground(Color.YELLOW);
+        displayLabel.setBackground(Color.gray);
         displayLabel.setOpaque(true);
+        displayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        displayLabel.setForeground(Color.white);
         jf.add(displayLabel);
+
+        JButton sevenButton = new JButton("7");
+        sevenButton.setBounds(30, 130, 80, 80);
+        jf.add(sevenButton);
+
+        JButton eightButton = new JButton("8");
+        eightButton.setBounds(130, 130, 80, 80);
+        jf.add(eightButton);
+
+        JButton nineButton = new JButton("9");
+        nineButton.setBounds(230, 130, 80, 80);
+        jf.add(nineButton);
+
+        JButton fourButton = new JButton("4");
+        fourButton.setBounds(30, 230, 80, 80);
+        jf.add(fourButton);
+
+        JButton fiveButton = new JButton("5");
+        fiveButton.setBounds(130, 230, 80, 80);
+        jf.add(fiveButton);
+
+        JButton sixButton = new JButton("6");
+        sixButton.setBounds(230, 230, 80, 80);
+        jf.add(sixButton);
+
+        JButton oneButton = new JButton("1");
+        oneButton.setBounds(30, 330, 80, 80);
+        jf.add(oneButton);
+
+        JButton twoButton = new JButton("2");
+        twoButton.setBounds(130, 330, 80, 80);
+        jf.add(twoButton);
+
+        JButton threeButton = new JButton("3");
+        threeButton.setBounds(230, 330, 80, 80);
+        jf.add(threeButton);
 
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close operation
         jf.setVisible(true);
